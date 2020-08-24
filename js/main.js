8784fb0d029bdecafc110230232eb62071696ea8
Ridwyn <salihouridwyn@gmail.com>
2020-08-24 13:06:05 +0100

    extension added

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@
 //   htmlTag.className = "slide-in";
 // }
 
+
 // SELECT DOM ITEMS
 const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
@@ -19,12 +20,50 @@ const navItems = document.querySelectorAll(".nav-item");
 // SET INITIAL STATE OF MENU
 let showMenu = false;
 
-{/* <li class="nav-item"><a href="/index.html" aria-label="Home" class="active">Home</a></li>
-<li class="nav-item"><a href="/about.html" aria-label="About Me">About Me</a></li>
-<li class="nav-item"><a href="/projects.html" aria-label="Projects">Projects</a></li>
-<li class="nav-item"><a href="/contact.html" aria-label="Contact">Get In Touch</a></li> */}
+let projects={
+ "webapps":[ 
+  {"href":{"github":"https://github.com/Ridwyn/MovieVue","live":"https://ridwyn.github.io/MovieVue/"},"src":{"png":"img/myprojects/project5.png","webp":"img/myprojects/project5.webp"},},
+  {"href":{"github":"https://github.com/Ridwyn/MicroBlog_flask_mongodb","live":"https://microblog2019.herokuapp.com/"},"src":{"png":"img/myprojects/project1.png","webp":"img/myprojects/project1.webp"},},
+  {"href":{"github":"https://github.com/Ridwyn/TravelAgencyTheme","live":"https://ridwyn.github.io/TravelAgencyTheme/"},"src":{"png":"img/myprojects/project2.png","webp":"img/myprojects/project2.webp"},},
+  {"href":{"github":"https://github.com/Ridwyn/restauranttheme","live":"https://ridwyn.github.io/restauranttheme/"},"src":{"png":"img/myprojects/project4.png","webp":"img/myprojects/project4.webp"},},
+  {"href":{"github":"https://github.com/Ridwyn/ridwyn.github.io","live":"https://ridwyn.github.io/#/"},"src":{"png":"img/myprojects/project6.png","webp":"img/myprojects/project6.webp"},},
+          ],
+"extensions":[ 
+            {"href":{"chrome":"https://chrome.google.com/webstore/detail/excite/fablabeblioinlnghalebdlabpdcfadd","moz":"https://addons.mozilla.org/en-GB/firefox/addon/excite/"},"src":{"png":"img/myprojects/project7.png","webp":"img/myprojects/project7.webp"},}
+      ]
+}
 
+if(window.location.pathname=="/projects.html"){
+  var projetcsUl = document.querySelector(".webapps");
+  var extensionsUl = document.querySelector(".extensions");
+  projetcsUl.innerHTML= projects.webapps.map((item)=>{
+    return ` 
+    <div class="item">
+        <a href="${item.href.live}" aria-label="LiveDemo"> 
+          <picture>
+            <source srcset="${item.src.webp}" type="image/webp">
+            <source srcset="${item.src.png}" type="image/png"> 
+            <img src="${item.src.png}" alt="Alt Text!">
+          </picture>
+        </a>
+        <a href="${item.href.live}" aria-label="Livedemo" target="_blank" class="btn light"><i class="fa fa-eye"></i>  Live Demo</a>
+        <a href="${item.href.github}" aria-label="Github" target="_blank" class="btn dark"><i class="fa fa-github"></i> Github</a>
+        </div>`
+  }).join(" ");
+  // extensions
+  extensionsUl.innerHTML=  projects.extensions.map((item)=>{
+    return ` 
+    <div class="item">
+          <picture>
+            <source srcset="${item.src.png}" type="image/png"> 
+            <img src="${item.src.png}" alt="Alt Text!">
+          </picture>
 
+        <a href="${item.href.chrome}" target="_blank" aria-label="Livedemo" class="btn light"><i class="fa fa-chrome"></i>  Chrome</a>
+        <a href="${item.href.moz}" target="_blank" aria-label="Github" class="btn dark"><i class="fa fa-firefox"></i> Mozilla</a>
+        </div>`
+  }).join(" ");
+}
 let menuItems=[
   {'href':'/index.html','text':'Home'},
   {'href':'/about.html','text':'About Me'},
